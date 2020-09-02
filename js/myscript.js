a70230f5e0084fde04d4ee20a4508bf19299b834
Dan Zins <danzins15@gmail.com>
2020-09-01 21:48:50 -0500

    Created separate CSS files, added JS files and started to code header section

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function showmenu()
+{
+    var x = document.getElementById('hamburgerUl');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
